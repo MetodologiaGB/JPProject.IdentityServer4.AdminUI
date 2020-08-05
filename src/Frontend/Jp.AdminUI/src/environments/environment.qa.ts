2440323dd169d1694d558c96ae7559457c885368
packetspy <packetspy@gmail.com>
2020-08-04 21:23:19 -0300

    Ajustes para qa

---
+++ src/Frontend/Jp.AdminUI/src/environments/environment.qa.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  ResourceServer: "https://auth-api-sso-qa.metodologiagb.com.br/",
+  IssuerUri: "https://auth-sso-qa.metodologiagb.com.br",
+  RequireHttps: true,
+  Uri: "https://auth-admin-qa.metodologiagb.com.br",
+  defaultTheme: "C",
+  version: "3.0.3",
+};
