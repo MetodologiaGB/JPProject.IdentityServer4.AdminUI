9f003b22d9b64e695508ea9c86bfe70ee44b465b
packetspy <packetspy@gmail.com>
2020-08-04 22:39:37 -0300

    Ajustando outros pipelines

---
+++ src/Frontend/Jp.AdminUI/src/environments/environment.hm.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  ResourceServer: "https://auth-api-sso-hm.metodologiagb.com.br/",
+  IssuerUri: "https://auth-sso-hm.metodologiagb.com.br",
+  RequireHttps: true,
+  Uri: "https://auth-admin-hm.metodologiagb.com.br",
+  defaultTheme: "C",
+  version: "3.0.3",
+};
---
+++ src/Frontend/Jp.AdminUI/src/environments/environment.pr.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  ResourceServer: "https://auth-api-sso.metodologiagb.com.br/",
+  IssuerUri: "https://auth-sso.metodologiagb.com.br",
+  RequireHttps: true,
+  Uri: "https://auth-admin.metodologiagb.com.br",
+  defaultTheme: "C",
+  version: "3.0.3",
+};
